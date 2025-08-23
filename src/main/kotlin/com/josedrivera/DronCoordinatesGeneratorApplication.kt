01869b51fad74e1ae22f9228d62aadd8cc873568
José Daniel Rivera <josed.rivera@udea.edu.co>
2025-08-23 18:27:21 -0500

    feat: primer commit del proyecto dron_coordinates_generator

---
+++ src/main/kotlin/com/josedrivera/DronCoordinatesGeneratorApplication.kt
@@ -0,0 +1,11 @@
+package com.josedrivera
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DronCoordinatesGeneratorApplication
+
+fun main(args: Array<String>) {
+    runApplication<DronCoordinatesGeneratorApplication>(*args)
+}
